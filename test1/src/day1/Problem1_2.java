9187ee5f26436b702c3a52cd5730917610ba6806
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-05 23:23:47 +0900

    end day1

---
+++ test1/src/day1/Problem1_2.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class Problem1_2 {
+
+    public static void main(String[] args) {
+        System.out.print("ABCDEFGHI\n");
+    }
+}
---
+++ test1/src/day1/Problem1_4.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class Problem1_4 {
+
+    public static void main(String[] args) {
+        System.out.println(1 + " + " + 2 + " + " + 3 + " = " + (1 + 2 + 3));
+    }
+}
