47bca55a9876c3eb37abc3b3b845765da8068fdc
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-06 22:51:48 +0900

    add Sample206

---
+++ test1/src/day2/Sample206.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Sample206 {
+
+    public static void main(String[] args) {
+        final int NUMBER = 100;
+        final String STRING = "Hoge";
+
+        System.out.println(NUMBER);
+        System.out.println(STRING);
+
+        // finalが付いた変数は値を変えられない
+        // NUMBER = 1;
+        // STRING = "Geho";
+    }
+}
