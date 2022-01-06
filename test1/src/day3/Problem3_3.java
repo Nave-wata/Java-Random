39a0e3394b2281a0574b204a7828f7329b0af06d
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-07 00:14:20 +0900

    Add Problem3_3

---
+++ test1/src/day3/Problem3_3.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class Problem3_3 {
+
+    public static void main(String[] args) {
+        int num = (int) (Math.random() * 100) + 1;
+
+        if (num < 50) {
+            System.out.println("50未満です．");
+        } else {
+            System.out.println("50以上です．");
+        }
+    }
+}
