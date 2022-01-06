aca501649759d310a801bdcbce10f277e75652d7
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-06 22:59:36 +0900

    Modify Problem2_1

--- test1/src/day2/problem2_1.java
+++ test1/src/day2/problem2_1.java
@@ -1,6 +1,6 @@
 package day2;
 
-public class problem2_1 {
+public class Problem2_1 {
 
     public static void main(String[] args) {
         int a = 5, b = 3;
@@ -8,5 +8,9 @@ public class problem2_1 {
         System.out.println("a = " + a);
         System.out.println("b = " + b);
         System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " % " + b + " = " + (a % b));
     }
 }
