da4a4d33ce3133ac333cc97449edb8e7b7e43c81
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-07 10:38:18 +0900

    Correct Problem3_2

--- test1/src/day3/Problem3_2.java
+++ test1/src/day3/Problem3_2.java
@@ -5,6 +5,8 @@ public class Problem3_2 {
     public static void main(String[] args) {
         int num = (int) (Math.random() * 10) + 1;
 
+        System.out.println("num = " + num);
+
         if (num != 1) {
             System.out.println("1ではありません．");
         } else {
