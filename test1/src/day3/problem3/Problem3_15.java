805f6f6adfc2549dace29dd0cb5322c61f81cfb7
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-07 19:14:16 +0900

    Add Problem3_15

---
+++ test1/src/day3/problem3/Problem3_15.java
@@ -0,0 +1,20 @@
+package day3.problem3;
+
+public class Problem3_15 {
+
+    public static void main(String[] args) {
+        int num = (int) (Math.random() * 100) + 1;
+
+        System.out.println("数値：" + num);
+
+        if (80 <= num) {
+            System.out.println("優");
+        } else if (70 <= num) {
+            System.out.println("良");
+        } else if (60 <= num) {
+            System.out.println("可");
+        } else {
+            System.out.println("不可");
+        }
+    }
+}
