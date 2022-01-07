4c22d6ce11986594eee048e828ed35e4883ab7b2
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-07 10:37:23 +0900

    Correct Problem3_1

--- test1/src/day3/Problem3_1.java
+++ test1/src/day3/Problem3_1.java
@@ -5,6 +5,8 @@ public class Problem3_1 {
     public static void main(String[] args) {
         int num = (int) (Math.random() * 10) + 1;
 
+        System.out.println("num = " + num);
+
         if (num >= 5) {
             System.out.println("5以上です．");
         } else {
