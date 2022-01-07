f2f8c726ceb88f40ce471e6da6e303eb3f8bea43
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-07 10:39:04 +0900

    Correct Problem3_3

--- test1/src/day3/Problem3_3.java
+++ test1/src/day3/Problem3_3.java
@@ -5,6 +5,8 @@ public class Problem3_3 {
     public static void main(String[] args) {
         int num = (int) (Math.random() * 100) + 1;
 
+        System.out.println("num = " + num);
+
         if (num < 50) {
             System.out.println("50未満です．");
         } else {
