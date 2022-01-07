320409a2586dba6ce8b44e79b704509cbd973bb4
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-07 19:09:38 +0900

    Add Problem3_14

---
+++ test1/src/day3/problem3/Problem3_14.java
@@ -0,0 +1,20 @@
+package day3.problem3;
+
+public class Problem3_14 {
+
+    public static void main(String[] args) {
+        int num1 = (int) (Math.random() * 10) + 1;
+        int num2 = (int) (Math.random() * 10) + 1;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        if (num1 < num2) {
+            System.out.println("num2の方が大きいです．");
+        } else if (num1 > num2) {
+            System.out.println("num1の方が大きいです．");
+        } else {
+            System.out.println("等しいです．");
+        }
+    }
+}
