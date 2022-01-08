1eccb79dcace8f9e5f8d4fc84375edfd18094d6f
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-08 22:27:11 +0900

    Add Problem4_13

---
+++ test1/src/day4/problem4/Problem4_13.java
@@ -0,0 +1,17 @@
+package day4.problem4;
+
+public class Problem4_13 {
+
+    public static void main(String[] args) {
+        int num = (int) (Math.random() * 10) + 1;
+        int i;
+
+        if (5 <= num) {
+            for (i = 0; i < num; i++) {
+                System.out.print("★ ");
+            }
+        } else {
+            System.out.print("発生した数値：" + num);
+        }
+    }
+}
