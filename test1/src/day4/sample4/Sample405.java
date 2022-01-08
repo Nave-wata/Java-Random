639fec45a7ac84e77c112ca12c81b85ebe9ce07e
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-08 18:11:15 +0900

    Add Sample405

---
+++ test1/src/day4/sample4/Sample405.java
@@ -0,0 +1,29 @@
+package day4.sample4;
+
+public class Sample405 {
+
+    public static void main(String[] args) {
+        int i;
+        int num = 1;
+
+        // while ループで実行
+        System.out.print("whileで実行：");
+        i = 1;
+
+        while (i < num) {
+            System.out.print("*");
+            i++;
+        }
+
+        System.out.println();
+
+        // do ~ whileループで実行
+        System.out.print("do ~ whileで実行：");
+        i = 1;
+
+        do {
+            System.out.print("*");
+            i++;
+        } while (i < num);
+    }
+}
