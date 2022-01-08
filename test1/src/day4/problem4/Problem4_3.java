11b81f8ee29709f466549d0c22d470a303130c05
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-08 21:51:59 +0900

    Add Problem4_3

---
+++ test1/src/day4/problem4/Problem4_3.java
@@ -0,0 +1,16 @@
+package day4.problem4;
+
+public class Problem4_3 {
+
+    public static void main(String[] args) {
+        int num = (int) (Math.random() * 10) + 1;
+        int i = 0;
+
+        System.out.println("数：" + num);
+
+        do {
+            System.out.print("■ ");
+            i++;
+        } while (i < num);
+    }
+}
