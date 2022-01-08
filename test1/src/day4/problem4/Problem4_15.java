995b097bece54541b43d53fe9477b3f81ace36ff
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-08 22:30:39 +0900

    Add Problem4_15

---
+++ test1/src/day4/problem4/Problem4_15.java
@@ -0,0 +1,16 @@
+package day4.problem4;
+
+public class Problem4_15 {
+
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 1; i <= 100; i++) {
+            if (i % 10 == 0) {
+                System.out.println(i + " ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
