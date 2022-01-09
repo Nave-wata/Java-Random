630620d3e472ae24fa2a1d1a1812c71022a60a7b
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-10 00:23:30 +0900

    Add Problem5_12

---
+++ test1/src/day5/problem5/Problem5_12.java
@@ -0,0 +1,22 @@
+package day5.problem5;
+
+public class Problem5_12 {
+
+    public static void main(String[] args) {
+        int data[][] = new int[3][3];
+        int i, j;
+
+        for (i = 0; i < data.length; i++) {
+            for (j = 0; j < data[i].length; j++) {
+                data[i][j] = (int) (Math.random() * 10) + 1;
+            }
+        }
+
+        for (i = 0; i < data.length; i++) {
+            for (j = 0; j < data[i].length; j++) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
