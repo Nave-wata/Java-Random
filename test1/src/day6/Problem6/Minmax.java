da027418367446189c5d7e2586ff645452032c1a
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-10 16:02:22 +0900

    Modify Minmax

--- test1/src/day6/Problem6/Minmax.java
+++ test1/src/day6/Problem6/Minmax.java
@@ -3,17 +3,23 @@ package day6.Problem6;
 public class Minmax {
 
     // 最大値の取得
-    public int max(int n1, int n2) {
-        if (n1 > n2) {
+    public int max(int n1, int n2, int n3) {
+        if (n2 < n1 && n3 < n1) {
             return n1;
         }
-        return n2;
+        if (n3 < n2) {
+            return n2;
+        }
+        return n3;
     }
 
-    public int min(int n1, int n2) {
-        if (n1 < n2) {
+    public int min(int n1, int n2, int n3) {
+        if (n1 < n2 && n1 < n3) {
             return n1;
         }
-        return n2;
+        if (n2 < n3) {
+            return n2;
+        }
+        return n3;
     }
 }
