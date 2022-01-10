664af4282689bcca4c79600f8406f83686715c95
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-10 16:02:36 +0900

    Modify Problem6_1

--- test1/src/day6/Problem6/Problem6_1.java
+++ test1/src/day6/Problem6/Problem6_1.java
@@ -4,9 +4,9 @@ public class Problem6_1 {
 
     public static void main(String[] args) {
         Minmax m = new Minmax();
-        int a = 4, b = 2;
+        int a = 6, b = 2, c = 4;
 
-        System.out.println(a + " と " + b + " のうち，最大のものは " + m.max(a, b));
-        System.out.println(a + " と " + b + " のうち，最小のものは " + m.min(a, b));
+        System.out.println(a + " と " + b + " と " + c + " のうち，最大のものは " + m.max(a, b, c));
+        System.out.println(a + " と " + b + " と " + c + " のうち，最小のものは " + m.min(a, b, c));
     }
 }
