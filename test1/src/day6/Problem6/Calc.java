e788a1697cf3f8c3f84cc3d4fa71e4ec6683ebf7
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-10 15:46:31 +0900

    Add Calc

---
+++ test1/src/day6/Problem6/Calc.java
@@ -0,0 +1,14 @@
+package day6.Problem6;
+
+public class Calc {
+
+    // 加算
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    // 減算
+    double sub(double a, double b) {
+        return a - b;
+    }
+}
