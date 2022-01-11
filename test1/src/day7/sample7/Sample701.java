a977b4df889a83155ac2c5f506acfdf2a17188f5
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-11 21:11:10 +0900

    Add Samle701

---
+++ test1/src/day7/sample7/Sample701.java
@@ -0,0 +1,19 @@
+package day7.sample7;
+
+public class Sample701 {
+
+    public static void main(String[] args) {
+        SampleClass02 s = new SampleClass02();
+
+        // method1()は，privateなので外部からはアクセスできない．
+        // s.method1();
+
+        // method2()呼び出し
+        s.method2();
+
+        // method3()呼び出し
+        s.method3();
+        // numは，privateフィールドなので，外部からはアクセスできない
+        // s.num = 1;
+    }
+}
