827cabc9926df654f06c49490c788be85e06f1b2
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-12 21:02:55 +0900

    Add SampleEx101.java

---
+++ test1/src/applied/day1/sample1/SampleEx101.java
@@ -0,0 +1,21 @@
+package applied.day1.sample1;
+
+public class SampleEx101 {
+
+    public static void main(String[] args) {
+        SampleClassEx01 s1, s2;
+
+        System.out.println("インスタンス数：" + SampleClassEx01.getNumberOfInstance());
+
+        // インスタンスを生成する
+        s1 = new SampleClassEx01(50); // コンストラクタ1を呼び出す
+        s2 = new SampleClassEx01(); // コンストラクタ2を呼び出す
+
+        // 値を取得して表示
+        System.out.println(s1.getValue());
+        System.out.println(s2.getValue());
+
+        // インスタンスの数を表示
+        System.out.println("インスタンス数：" + SampleClassEx01.getNumberOfInstance());
+    }
+}
