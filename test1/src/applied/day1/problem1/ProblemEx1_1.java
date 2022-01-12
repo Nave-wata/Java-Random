bbbb1da803711a44f5787baa0e4996326da140fd
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-12 21:32:17 +0900

    Modify ProblemEx1_1.java

--- test1/src/applied/day1/problem1/ProblemEx1_1.java
+++ test1/src/applied/day1/problem1/ProblemEx1_1.java
@@ -5,14 +5,20 @@ public class ProblemEx1_1 {
     public static void main(String[] args) {
         int a = (int) (Math.random() * 10) + 1;
         int b = (int) (Math.random() * 10) + 1;
-        int result = add(a, b);
+        int ans_add = add(a, b);
+        int ans_sub = sub(a, b);
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-        System.out.println(a + " + " + b + " = " + result);
+        System.out.println(a + " + " + b + " = " + ans_add);
+        System.out.println(a + " - " + b + " = " + ans_sub);
     }
 
     static int add(int a, int b) {
         return a + b;
+    }
+
+    static int sub(int a, int b) {
+        return a - b;
     }
 }
