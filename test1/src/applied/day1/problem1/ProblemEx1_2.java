17b264480b5d7dac5f4267c843d4daacff5eafbb
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-12 21:37:36 +0900

    Modify ProblemEx1_2.java

--- test1/src/applied/day1/problem1/ProblemEx1_2.java
+++ test1/src/applied/day1/problem1/ProblemEx1_2.java
@@ -5,14 +5,16 @@ public class ProblemEx1_2 {
     public static void main(String[] args) {
         int a = (int) (Math.random() * 10) + 1;
         int b = (int) (Math.random() * 10) + 1;
-        int result = mul(a, b);
+        int c = (int) (Math.random() * 10) + 1;
+        int result = mul(a, b, c);
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-        System.out.println(a + " * " + b + " = " + result);
+        System.out.println("c = " + c);
+        System.out.println(a + " * " + b + " * " + c + " = " + result);
     }
 
-    static int mul(int a, int b) {
-        return a * b;
+    static int mul(int a, int b, int c) {
+        return a * b * c;
     }
 }
