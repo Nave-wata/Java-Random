91794dd91015bc34a28a024c849ae1609f94674c
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-13 21:32:42 +0900

    FuncCalc.java

---
+++ test1/src/applied/day2/problem2/FundCalc.java
@@ -0,0 +1,30 @@
+package applied.day2.problem2;
+
+public class FundCalc {
+    private double number1 = 0.0;
+    private double number2 = 0.0;
+
+    public void setNumber1(double number1) {
+        this.number1 = number1;
+    }
+
+    public void setNumber2(double number2) {
+        this.number2 = number2;
+    }
+
+    public double getNumber1() {
+        return number1;
+    }
+
+    public double getNumber2() {
+        return number2;
+    }
+
+    public double add() {
+        return number1 + number2;
+    }
+
+    public double sub() {
+        return number1 - number2;
+    }
+}
