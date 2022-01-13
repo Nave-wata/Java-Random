10eb83887724fe04593faeea0f9132414a271bda
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-13 21:35:16 +0900

    Add NewCalc.java

---
+++ test1/src/applied/day2/problem2/NewCalc.java
@@ -0,0 +1,11 @@
+package applied.day2.problem2;
+
+public class NewCalc extends FundCalc {
+    public double mul() {
+        return number1 * number2;
+    }
+
+    public double div() {
+        return number1 / number2;
+    }
+}
