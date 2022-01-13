e15ed0ca88a84becc3611995db10399eb14f63e9
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-13 21:29:36 +0900

    Add ProblemEx2_1.java

---
+++ test1/src/applied/day2/problem2/ProblemEx2_1.java
@@ -0,0 +1,14 @@
+package applied.day2.problem2;
+
+public class ProblemEx2_1 {
+
+    public static void main(String[] args) {
+        Fighter f = new Fighter();
+        Airplane a = new Airplane();
+
+        a.fly();
+
+        f.fly();
+        f.fight();
+    }
+}
