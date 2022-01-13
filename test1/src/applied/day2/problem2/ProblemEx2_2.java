cb5d40942279c7466d3738815bfe79c609911476
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-13 21:39:43 +0900

    Add ProblemEx2_2.java

---
+++ test1/src/applied/day2/problem2/ProblemEx2_2.java
@@ -0,0 +1,15 @@
+package applied.day2.problem2;
+
+public class ProblemEx2_2 {
+    public static void main(String[] args) {
+        NewCalc n = new NewCalc();
+
+        n.setNumber1(10);
+        n.setNumber2(2);
+
+        System.out.println(n.getNumber1() + " + " + n.getNumber2() + " = " + n.add());
+        System.out.println(n.getNumber1() + " - " + n.getNumber2() + " = " + n.sub());
+        System.out.println(n.getNumber1() + " * " + n.getNumber2() + " = " + n.mul());
+        System.out.println(n.getNumber1() + " / " + n.getNumber2() + " = " + n.div());
+    }
+}
