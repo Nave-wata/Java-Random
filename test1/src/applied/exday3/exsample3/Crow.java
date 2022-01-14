7d32a266c8f103321c8efa53be3f7a19424ccb7b
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-14 22:49:56 +0900

    Add Crow.java

---
+++ test1/src/applied/exday3/exsample3/Crow.java
@@ -0,0 +1,13 @@
+package applied.exday3.exsample3;
+
+public class Crow {
+    private String name = "カラス";
+
+    public String getName() {
+        return name;
+    }
+
+    public void sing() {
+        System.out.println("カーカー");
+    }
+}
