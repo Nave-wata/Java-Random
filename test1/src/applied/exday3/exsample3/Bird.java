468e52ca23430e2dcba71d4887d28852e5425691
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-14 22:58:23 +0900

    Add Bird.java

---
+++ test1/src/applied/exday3/exsample3/Bird.java
@@ -0,0 +1,13 @@
+package applied.exday3.exsample3;
+
+public abstract class Bird {
+    private String name;
+
+    Bird(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
