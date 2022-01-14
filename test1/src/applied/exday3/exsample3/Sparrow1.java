f955aa7cf281300428cd294db7c87aaa887c0b8f
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-14 22:51:05 +0900

    Add Saparrow1.java

---
+++ test1/src/applied/exday3/exsample3/Sparrow1.java
@@ -0,0 +1,13 @@
+package applied.exday3.exsample3;
+
+public class Sparrow1 {
+    private String name = "すずめ";
+
+    public String getName() {
+        return name;
+    }
+
+    public void sing() {
+        System.out.println("チュンチュン");
+    }
+}
