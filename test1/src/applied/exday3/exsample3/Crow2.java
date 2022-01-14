1ac84bc8fe0bb452e3ca7aa073743a7da9a4ee18
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-14 23:04:56 +0900

    Add Crow2.java

---
+++ test1/src/applied/exday3/exsample3/Crow2.java
@@ -0,0 +1,11 @@
+package applied.exday3.exsample3;
+
+public class Crow2 extends Bird {
+    public Crow2() {
+        super("カラス");
+    }
+
+    public void sing() {
+        System.out.println("カーカー");
+    }
+}
