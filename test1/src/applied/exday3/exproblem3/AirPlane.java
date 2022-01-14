3805e29cfc12fee7556e92c7e7bee7268a25adbf
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-14 23:41:11 +0900

    Add AirPlane.java

---
+++ test1/src/applied/exday3/exproblem3/AirPlane.java
@@ -0,0 +1,15 @@
+package applied.exday3.exproblem3;
+
+public abstract class AirPlane {
+    private String type;
+
+    AirPlane(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    abstract String fly();
+}
