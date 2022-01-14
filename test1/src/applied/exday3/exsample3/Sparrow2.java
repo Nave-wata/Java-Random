c71032494fcc17eaf3545f3bae73fb98292f39de
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-14 23:03:45 +0900

    Add Sparrow2.java

---
+++ test1/src/applied/exday3/exsample3/Sparrow2.java
@@ -0,0 +1,11 @@
+package applied.exday3.exsample3;
+
+public class Sparrow2 extends Bird {
+    public Sparrow2() {
+        super("すずめ"); // Birdクラスの引数付きコンストラクタを呼び出す
+    }
+
+    public void sing() {
+        System.out.println("チュンチュン");
+    }
+}
