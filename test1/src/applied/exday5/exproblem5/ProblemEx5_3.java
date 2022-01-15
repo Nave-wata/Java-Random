fcfca2daf241c0c13ef5ba72262841c4b8d3e0f3
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-15 21:25:43 +0900

    Correct Problem5_3.java

--- test1/src/applied/exday5/exproblem5/ProblemEx5_3.java
+++ test1/src/applied/exday5/exproblem5/ProblemEx5_3.java
@@ -4,24 +4,30 @@ import java.util.*;
 
 public class ProblemEx5_3 {
     public static void main(String[] args) {
-        ArrayList<Integer> array = new ArrayList<Integer>();
-        int cnt_num[] = new int[10];
+        ArrayList<Integer> arys = new ArrayList<Integer>();
         int num = (int) (Math.random() * 100) + 1 - 1;
         int i;
+        boolean flag;
 
         while (0 != num) {
-            array.add(num);
-            i = num % 10;
-            cnt_num[i] += 1;
+            arys.add(num);
             num = (int) (Math.random() * 100) + 1 - 1;
             System.out.println("0 ~ 10の値を出力：" + num);
         }
 
-        for (i = 0; i < cnt_num.length; i++) {
-            if (cnt_num[i] != 0) {
-                System.out.println("一の位が" + i + "：" + cnt_num[i]);
+        for (i = 0; i < 10; i++) {
+            flag = true;
+            System.out.print("一の位が" + i + "：");
+            for (Integer j : arys) {
+                if (j % 10 == i) {
+                    System.out.print(j + " ");
+                    flag = false;
+                }
+            }
+            if (flag) {
+                System.out.println("なし");
             } else {
-                System.out.println("一の位が" + i + "：" + "なし");
+                System.out.println();
             }
         }
     }
