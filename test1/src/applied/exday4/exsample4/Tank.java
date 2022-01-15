4dbea3d741bb6eec6f6c35fe2d146fe0ab380460
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-15 11:35:24 +0900

    Modify SampleEx402.java, Tank.java

--- test1/src/applied/exday4/exsample4/Tank.java
+++ test1/src/applied/exday4/exsample4/Tank.java
@@ -14,7 +14,7 @@ public class Tank extends Weapon implements ICar, ICannon {
     }
 
     public void attack() {
-        System.out.println(this.getName() + " の攻撃方法");
+        System.out.println(this.getName() + "の攻撃方法");
         System.out.println("---------------");
         fire();
         move();
