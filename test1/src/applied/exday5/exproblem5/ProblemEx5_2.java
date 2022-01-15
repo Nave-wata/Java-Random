0b3b5dc0faf3dd46de454eff08181caa5ddd1e55
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-15 14:46:42 +0900

    Modify ProblemEx5_2.java

--- test1/src/applied/exday5/exproblem5/ProblemEx5_2.java
+++ test1/src/applied/exday5/exproblem5/ProblemEx5_2.java
@@ -5,13 +5,12 @@ import java.util.*;
 public class ProblemEx5_2 {
     public static void main(String[] args) {
         ArrayList<Integer> array = new ArrayList<Integer>();
-        int num = 1;
+        int num = (int) (Math.random() * 10) + 1 - 1;
         int i;
 
         while (0 != num) {
-            num = (int) (Math.random() * 10) + 1 - 1;
             array.add(num);
-
+            num = (int) (Math.random() * 10) + 1 - 1;
             System.out.println("0 ~ 10の値を出力：" + num);
         }
 
