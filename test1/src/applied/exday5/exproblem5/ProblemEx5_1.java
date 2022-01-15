8fe264445aa01a0f28fd11374b5b8a74e9aa34d7
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-15 13:21:54 +0900

    Add ProblemEx5_1.java

---
+++ test1/src/applied/exday5/exproblem5/ProblemEx5_1.java
@@ -0,0 +1,31 @@
+package applied.exday5.exproblem5;
+
+import java.util.*;
+
+public class ProblemEx5_1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        int num = 1;
+
+        while (0 != num) {
+            num = (int) (Math.random() * 10) + 1 - 1;
+            array.add(num);
+
+            System.out.println("0 ~ 10の値を出力：" + num);
+        }
+
+        System.out.print("偶数：");
+        for (Integer i : array) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.print("\n奇数：");
+        for (Integer i : array) {
+            if (i % 2 == 1) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
