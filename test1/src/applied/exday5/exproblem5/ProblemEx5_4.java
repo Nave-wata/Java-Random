1dd9a7147061981f98cf09d03bf6d9e94f20985b
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-15 21:36:23 +0900

    Add Problem5_4.java

---
+++ test1/src/applied/exday5/exproblem5/ProblemEx5_4.java
@@ -0,0 +1,13 @@
+package applied.exday5.exproblem5;
+
+import java.io.*;
+
+public class ProblemEx5_4 {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String buf = br.readLine();
+
+        System.out.println(buf);
+    }
+}
