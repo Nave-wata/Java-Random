beb91253001fce0268659ac50764f88d4492fc47
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-15 14:42:15 +0900

    tmp ProblemEx5_2.java

---
+++ test1/src/applied/exday5/exproblem5/ProblemEx5_2.java
@@ -0,0 +1,23 @@
+package applied.exday5.exproblem5;
+
+import java.util.*;
+
+public class ProblemEx5_2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        int num = 1;
+        int i;
+
+        while (0 != num) {
+            num = (int) (Math.random() * 10) + 1 - 1;
+            array.add(num);
+
+            System.out.println("0 ~ 10の値を出力：" + num);
+        }
+
+        for (i = array.size() - 1; 0 <= i; i--) {
+            int d = array.get(i);
+            System.out.print(d + " ");
+        }
+    }
+}
