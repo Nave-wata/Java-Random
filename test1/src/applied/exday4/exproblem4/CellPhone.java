f5b51df4a282c1db3795a3468b1dfdd245f51f9d
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-15 11:42:47 +0900

    Add CellPhone.java

---
+++ test1/src/applied/exday4/exproblem4/CellPhone.java
@@ -0,0 +1,23 @@
+package applied.exday4.exproblem4;
+
+public class CellPhone {
+    public void sendMail() {
+        System.out.println("メールを送る");
+    }
+
+    public void recieveMail() {
+        System.out.println("メールを受信する");
+    }
+
+    public void playGame() {
+        System.out.println("ウェブを閲覧する");
+    }
+
+    public void callPhon() {
+        System.out.println("電話をかける");
+    }
+
+    public void recievePhone() {
+        System.out.println("電話を受ける");
+    }
+}
