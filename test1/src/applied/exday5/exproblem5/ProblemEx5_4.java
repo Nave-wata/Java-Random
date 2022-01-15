0177968e2b0783cd213f814941e5b41168976c08
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-15 21:52:56 +0900

    Modify ProblemEx5_4.java

--- test1/src/applied/exday5/exproblem5/ProblemEx5_4.java
+++ test1/src/applied/exday5/exproblem5/ProblemEx5_4.java
@@ -1,13 +1,25 @@
 package applied.exday5.exproblem5;
 
 import java.io.*;
+import java.util.*;
 
 public class ProblemEx5_4 {
     public static void main(String[] args) throws IOException {
+        ArrayList<String> ary = new ArrayList<String>();
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
+
+        System.out.print("文字列を入力：");
         String buf = br.readLine();
 
-        System.out.println(buf);
+        while (buf != "") {
+            ary.add(buf);
+            System.out.print("文字列を入力：");
+            buf = br.readLine(); // ここで改行入る
+        }
+
+        for (String s : ary) {
+            System.out.print(s + " ");
+        }
     }
 }
