9d8fb24f860858202655a434f8e4751443ba8f97
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-16 14:30:45 +0900

    Add SampleEx603.java

---
+++ test1/src/applied/exday6/exsample6/SampleEx603.java
@@ -0,0 +1,33 @@
+package applied.exday6.exsample6;
+
+public class SampleEx603 {
+    public static void main(String[] args) {
+        String s1 = "ABCDEF";
+        String s2 = "ABCDEF";
+        String s3 = new String("ABCDEF");
+
+        if (s1.equals(s2)) {
+            System.out.println("s1とs2の文字列は等しい"); // pass
+        } else {
+            System.out.println("s1とs2の文字列は等しくない");
+        }
+
+        if (s1.equals(s3)) {
+            System.out.println("s1とs3の文字列は等しい"); // pass
+        } else {
+            System.out.println("s1とs3の文字列は等しくない");
+        }
+
+        if (s1 == s2) {
+            System.out.println("s1とs2のオブジェクトは等しい"); // pass
+        } else {
+            System.out.println("s1とs2のオブジェクトは等しくない");
+        }
+
+        if (s1 == s3) {
+            System.out.println("s1とs3のオブジェクトは等しい");
+        } else {
+            System.out.println("s1とs3のオブジェクトは等しくない"); // pass
+        }
+    }
+}
