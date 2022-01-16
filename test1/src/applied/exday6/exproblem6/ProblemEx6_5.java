9549839faa6ac8e10fee9c67981442b3c2d36944
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-16 16:58:22 +0900

    Add ProblemEx6_5.java

---
+++ test1/src/applied/exday6/exproblem6/ProblemEx6_5.java
@@ -0,0 +1,15 @@
+package applied.exday6.exproblem6;
+
+public class ProblemEx6_5 {
+    public static void main(String[] args) {
+        String s1 = "hoge";
+        String s2 = "hoge";
+        // String s2 = new String("hoge");
+
+        if (s1 == s2) {
+            System.out.println(s1 + "と" + s2 + "は，同じ文字列です");
+        } else {
+            System.out.println(s1 + "と" + s2 + "は，同じ文字列ではありません");
+        }
+    }
+}
