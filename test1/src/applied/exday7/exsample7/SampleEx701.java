31b0f56e475ef454d08b85bdb947cfea41ac397c
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-17 18:57:13 +0900

    Add SampleEx701.java

---
+++ test1/src/applied/exday7/exsample7/SampleEx701.java
@@ -0,0 +1,10 @@
+package applied.exday7.exsample7;
+
+public class SampleEx701 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 5;
+
+        System.out.println(b / a); // 0で除算しているためエラーが出る
+    }
+}
