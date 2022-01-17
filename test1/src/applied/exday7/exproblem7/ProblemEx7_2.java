fd5af642b4b9c17950392866f78f174cd56d7280
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-17 19:46:25 +0900

    Add ProblemEx7_2.java

---
+++ test1/src/applied/exday7/exproblem7/ProblemEx7_2.java
@@ -0,0 +1,12 @@
+package applied.exday7.exproblem7;
+
+public class ProblemEx7_2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(i + "/ " + j + " = " + i / j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
