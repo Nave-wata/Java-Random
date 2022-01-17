f8b50e1b93e40972a70273c3ab3299a8b184f47b
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-17 19:37:44 +0900

    Modify ProblemEx7_1.java

--- test1/src/applied/exday7/exproblem7/ProblemEx7_1.java
+++ test1/src/applied/exday7/exproblem7/ProblemEx7_1.java
@@ -4,8 +4,12 @@ public class ProblemEx7_1 {
     public static void main(String[] args) {
         int a[] = { 0, 1, 2 };
 
-        for (int i = 0; i < 4; i++) {
-            System.out.println("a[" + i + "] = " + a[i]);
+        try {
+            for (int i = 0; i < 4; i++) {
+                System.out.println("a[" + i + "] = " + a[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("配列の範囲を超えています");
         }
     }
 }
