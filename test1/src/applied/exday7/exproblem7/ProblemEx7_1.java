50556768619a423af57db92633dd7cf6a8777805
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-17 19:35:32 +0900

    Add ProblemEx7_1.java

---
+++ test1/src/applied/exday7/exproblem7/ProblemEx7_1.java
@@ -0,0 +1,11 @@
+package applied.exday7.exproblem7;
+
+public class ProblemEx7_1 {
+    public static void main(String[] args) {
+        int a[] = { 0, 1, 2 };
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("a[" + i + "] = " + a[i]);
+        }
+    }
+}
