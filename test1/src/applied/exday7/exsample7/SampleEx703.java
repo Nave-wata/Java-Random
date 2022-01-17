1e7874f9b545b403901990b3736434fcd4c1e9a0
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-17 19:16:08 +0900

    Add SampleEx703.java

---
+++ test1/src/applied/exday7/exsample7/SampleEx703.java
@@ -0,0 +1,32 @@
+package applied.exday7.exsample7;
+
+public class SampleEx703 {
+    public static void main(String[] args) {
+        try {
+            for (int i = 0; i <= 5; i++) {
+                int a = getNum(i);
+                int b = 5;
+
+                System.out.print(a + " / " + b + " = ");
+                System.out.println(calc(a, b));
+            }
+        } catch (ArithmeticException e) {
+            System.out.println();
+            System.out.println("0による除算発生");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("配列の範囲外にアクセスしました");
+        } finally {
+            System.out.println("終了");
+        }
+    }
+
+    private static int calc(int a, int b) throws ArithmeticException {
+        return a / b;
+    }
+
+    public static int getNum(int index) throws ArrayIndexOutOfBoundsException {
+        int num[] = { 1, 2, 3, 4 };
+
+        return num[index];
+    }
+}
