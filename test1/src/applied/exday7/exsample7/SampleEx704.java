4ddd54cc07825b348cc48d7632a4d58d4cb53d98
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-17 19:33:07 +0900

    SampleEx704.java mine

--- test1/src/applied/exday7/exsample7/SampleEx704.java
+++ test1/src/applied/exday7/exsample7/SampleEx704.java
@@ -3,9 +3,15 @@ package applied.exday7.exsample7;
 public class SampleEx704 {
     public static void main(String[] args) {
         try {
-            throw new SampleException("自作の例外のサンプル"); // 故意に例外を発生させる
+            for (int i = 0; i < 10; i++) {
+                if (i == 5) {
+                    throw new SampleException("i = " + i + "例外風に"); // 故意に例外を発生させる
+                } else {
+                    System.out.println(i);
+                }
+            }
         } catch (SampleException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // この行書かなくてもi = 5以降は出力されない(エラーはでない)
         }
     }
 }
