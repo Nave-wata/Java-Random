5bc03227eb6750de59afc0148f0616edbdbd508f
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-17 19:23:03 +0900

    Add SampleException.java

---
+++ test1/src/applied/exday7/exsample7/SampleException.java
@@ -0,0 +1,9 @@
+package applied.exday7.exsample7;
+
+public class SampleException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public SampleException(String message) {
+        super(message);
+    }
+}
