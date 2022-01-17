b2fbc7728239c2df8812a8af289195ee67d4cb38
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-17 19:53:52 +0900

    例外処理追加

--- test1/src/applied/exday7/exproblem7/ProblemEx7_2.java
+++ test1/src/applied/exday7/exproblem7/ProblemEx7_2.java
@@ -2,11 +2,15 @@ package applied.exday7.exproblem7;
 
 public class ProblemEx7_2 {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(i + "/ " + j + " = " + i / j + " ");
+        try {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(i + " / " + j + " = " + i / j + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
+        } catch (ArithmeticException e) {
+            System.out.println("0で割り算はできません");
         }
     }
 }
