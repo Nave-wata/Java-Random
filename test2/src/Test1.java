0a1ac0dd2a0e61829839bd17a151ac2c374f64a6
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-31 12:41:59 +0900

    tirmは前後の空白だけ削除

---
+++ test2/src/Test1.java
@@ -0,0 +1,6 @@
+public class Test1 {
+    public static void main(String[] args) {
+        String str = "   A B C    ";
+        System.out.println(str.trim());
+    }
+}
--- test2/src/TypeJdg.java
+++ test2/src/TypeJdg.java
@@ -1,7 +1,7 @@
 public class TypeJdg {
     public static void main(String[] args) {
         String str = "1";
-        boolean flag = false;
+        int num = 1;
 
         if (str.matches("(1|2|3|4|5|6|7|8|9|0)")) {
             System.out.println("数字オンリー");
