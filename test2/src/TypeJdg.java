8a7e00309c2ad91987925b4bbb5d240ee9e77f2c
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-31 10:14:52 +0900

    見つけられた

---
+++ test2/src/TypeJdg.java
@@ -0,0 +1,12 @@
+public class TypeJdg {
+    public static void main(String[] args) {
+        String str = "Sample String1";
+        boolean flag = false;
+
+        if (str.contains("1")) {
+            System.out.println("!");
+        } else {
+
+        }
+    }
+}
