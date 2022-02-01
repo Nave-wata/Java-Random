d50c7646e87944709d964317b11bfdb07eaaf4ea
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-01 18:59:00 +0900

    行けそう ただし内外public

---
+++ test2/src/Call.java
@@ -0,0 +1,6 @@
+public class Call {
+    public static void main(String[] args) {
+        PubProPri a = new PubProPri();
+        PubProPri.Pri b = a.Pri();
+    }
+}
---
+++ test2/src/PubProPri.java
@@ -0,0 +1,12 @@
+public class PubProPri {
+    public class Pri {
+        public Pri() {
+        }
+
+        public String Name = "Nave-wata";
+    }
+
+    public PubProPri.Pri Pri() {
+        return null;
+    }
+}
