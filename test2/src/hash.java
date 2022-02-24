7e8dc0170c1c2689698ecca16b0315e356f04355
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-25 01:16:59 +0900

    C++に決定． ストレッチングはとんでもない数（数千から数万回）ハッシュ化するらしいから

--- test2/src/hash.java
+++ test2/src/hash.java
@@ -4,15 +4,20 @@ import java.security.NoSuchAlgorithmException;
 
 public class hash {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         String etPass = "hoge";
         MessageDigest sha256 = null;
-        try {
-            sha256 = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 100; i++) {
+
+            try {
+                sha256 = MessageDigest.getInstance("SHA-256");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+            byte[] sha256_result = sha256.digest(etPass.getBytes());
+            String result = String.format("%x04", new BigInteger(1, sha256_result));
         }
-        byte[] sha256_result = sha256.digest(etPass.getBytes());
-        String result = String.format("%x04", new BigInteger(1, sha256_result));
-        System.out.println("result: " + result);
+        long end = System.currentTimeMillis();
+        System.out.println("result: " + (end - start));
     }
 }
