e3a734bf77dd7cfd9c8c9954325219587fec5949
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-25 01:34:04 +0900

    特になし

--- test2/src/hash.java
+++ test2/src/hash.java
@@ -1,23 +1,26 @@
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 public class hash {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         String etPass = "hoge";
         MessageDigest sha256 = null;
-        for (int i = 0; i < 100; i++) {
-
-            try {
-                sha256 = MessageDigest.getInstance("SHA-256");
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            }
-            byte[] sha256_result = sha256.digest(etPass.getBytes());
-            String result = String.format("%x04", new BigInteger(1, sha256_result));
+        try {
+            sha256 = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
+
+        for (int i = 0; i < 10000; i++) {
+            byte[] sha256_result = sha256.digest(etPass.getBytes());
+            etPass = String.format("%x04", new BigInteger(1, sha256_result));
+        }
+        System.out.println(etPass);
+
         long end = System.currentTimeMillis();
-        System.out.println("result: " + (end - start));
+        System.out.println("result: " + ((double) (end - start) / 1000));
     }
 }
