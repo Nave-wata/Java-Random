108c0ea6b7bac613721d23fef2d6397e5f095654
Nave3 <90523045+Nave-wata@users.noreply.github.com>
2022-09-18 19:01:22 +0900

    Create Inputs.java

---
+++ test3/Inputs.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Input {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    // Input: A B C, Output: A
+    String next = scanner.next();
+    
+    // Input: A B C, Output: A B C
+    String nextLine = scanner.nextLine();
+  }
+}
