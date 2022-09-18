92d699e42ad5fdd9ba404f8628739a23aec6beab
Nave3 <90523045+Nave-wata@users.noreply.github.com>
2022-09-18 19:02:11 +0900

    Update Inputs.java

--- test3/Inputs.java
+++ test3/Inputs.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Input {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
     // Input: A B C, Output: A
